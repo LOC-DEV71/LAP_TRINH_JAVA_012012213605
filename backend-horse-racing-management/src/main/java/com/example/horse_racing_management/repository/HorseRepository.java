77c347b2eaeb3f2b941fcd10c0e0ec0f803167b0
Nhat_dep <96358505+Nhat-Dep@users.noreply.github.com>
2026-06-16 13:50:57 +0700

    update HorseRepository

--- backend-horse-racing-management/src/main/java/com/example/horse_racing_management/repository/HorseRepository.java
+++ backend-horse-racing-management/src/main/java/com/example/horse_racing_management/repository/HorseRepository.java
@@ -1,9 +1,11 @@
 package com.example.horse_racing_management.repository;
 
 import com.example.horse_racing_management.entity.Horse;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface HorseRepository extends MongoRepository<Horse, String> {
     // Spring Data sẽ cung cấp sẵn các hàm như:
